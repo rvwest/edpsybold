be245af7e2459a09a09a087b428d6da5b053159d
rvwest <github@rvwest.co.uk>
2025-03-19 23:20:09 +0000

    Jobs

--- job_manager/job-filters.php
+++ job_manager/job-filters.php
@@ -20,6 +20,12 @@ wp_enqueue_script('wp-job-manager-ajax-filters');
 do_action('job_manager_job_filters_before', $atts);
 ?>
 
+<form class="job_filters">
+	<?php do_action('job_manager_job_filters_start', $atts); ?>
+
+	<?php do_action('job_manager_job_filters_end', $atts); ?>
+</form>
+
 <?php do_action('job_manager_job_filters_after', $atts); ?>
 
 <noscript><?php esc_html_e('Your browser does not support JavaScript, or it is disabled. JavaScript must be enabled in order to view listings.', 'wp-job-manager'); ?></noscript>
