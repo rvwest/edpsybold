73f3d2ae0970109110cc2ec035d6107f1d1bd632
rvwest <github@rvwest.co.uk>
2025-07-12 13:18:59 +0100

    Add mobile submenu toggle buttons

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -4,4 +4,23 @@ jQuery(function($){
         nav.toggleClass('open');
         $(this).find('i').toggleClass('fa-bars fa-times');
     });
+
+    // Insert submenu toggle buttons
+    $('.top-nav-menu li.menu-item-has-children').each(function(){
+        var $li = $(this);
+        if(!$li.children('button.submenu-toggle').length){
+            var $btn = $('<button class="submenu-toggle" aria-expanded="false"><i class="far fa-angle-down"></i></button>');
+            $li.prepend($btn);
+        }
+    });
+
+    // Toggle submenu visibility
+    $('.top-nav-menu').on('click', 'button.submenu-toggle', function(e){
+        e.preventDefault();
+        var $btn = $(this);
+        var $li = $btn.parent('li');
+        $li.toggleClass('submenu-open');
+        $li.children('ul.sub-menu').slideToggle(200);
+        $btn.find('i').toggleClass('fa-angle-down fa-angle-up');
+    });
 });
