5dbee06db840ac8a9f9539dd3a72eff80cca315c
rvwest <github@rvwest.co.uk>
2025-08-24 15:36:44 +0100

    menu fix

--- _promo-banner.php
+++ _promo-banner.php
@@ -12,19 +12,18 @@ $icon  = get_theme_mod('promo_banner_icon', '');
 $text1 = get_theme_mod('promo_banner_text1', '');
 $text2 = get_theme_mod('promo_banner_text2', '');
 ?>
-<div class="cta-header-block <?php echo esc_attr( $class ); ?>">
-    <div class="container">
-        <div class="row-fluid fixedrow">
+<div class="promo-banner-block edp-fullwidth <?php echo esc_attr( $class ); ?>">
+
+        <div class="promo-banner-wrapper grid12">
             <p>
                 <?php if ( $icon ) : ?>
                     <i class="<?php echo esc_attr( $icon ); ?>"></i>&nbsp;
                 <?php endif; ?>
-                Come join us: <?php echo wp_kses_post( $text1 ); ?>
+            <?php echo wp_kses_post( $text1 ); ?>
             </p>
             <?php if ( $text2 ) : ?>
                 <p class="cta-second-link"><?php echo wp_kses_post( $text2 ); ?></p>
             <?php endif; ?>
-        </div>
-    </div>
+            </div>
 </div>
 <?php echo '<!-- file end: _promo-banner.php -->'; ?>
