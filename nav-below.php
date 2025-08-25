228073c9f2a82712ce24505b0b472bf899672301
rvwest <github@rvwest.co.uk>
2025-08-25 10:20:41 +0100

    Listing nav

--- nav-below.php
+++ nav-below.php
@@ -1,7 +1,9 @@
 <!-- file: nav-below.php -->
-<?php $args = array(
-    'prev_text' => sprintf(esc_html__('%s older', 'edpsybold'), '<span class="meta-nav">&larr;</span>'),
-    'next_text' => sprintf(esc_html__('newer %s', 'edpsybold'), '<span class="meta-nav">&rarr;</span>')
+<?php
+$args = array(
+    'prev_text' => '<a class="edp-button-outline button">' . sprintf(esc_html__('%s older', 'edpsybold'), '<i class="fas fa-arrow-left"></i>') . '</a>',
+    'next_text' => '<a class="edp-button-outline button">' . sprintf(esc_html__('newer %s', 'edpsybold'), '<i class="fas fa-arrow-right"></i>') . '</a>',
+    'screen_reader_text' => ' '
 );
 the_posts_navigation($args);
 echo '<!-- file end: nav-below.php -->';
