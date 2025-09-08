f908205defd6f63765ba3a89b329d8bbed415bd8
rvwest <github@rvwest.co.uk>
2025-09-08 14:28:21 +0100

    Tag

--- entry-footer.php
+++ entry-footer.php
@@ -1,14 +1,15 @@
 <!-- file: entry-footer.php -->
- <hr>
+
 <footer class="entry-footer grid12">
   
-    <span class="tag-links"><?php
-$before = '<div class="tag-title">Tags:</div><div class="tag-items">';
+    <div class="tag-links"><?php
+$before = '<i class="far fa-tags"></i><div class="tag-items">';
 $seperator = ''; // blank instead of comma
 $after = '</div>';
 
 the_tags( $before, $seperator, $after );
-?></span>
+?></div>
+<div class="page-divider"> </div>
     <?php get_template_part('biography', get_post_format()); ?>
 </footer>
 <!-- file end: entry-footer.php -->
