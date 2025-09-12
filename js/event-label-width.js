c4b62467db2633d8c82b715305739575c8acf623
rvwest <github@rvwest.co.uk>
2025-09-12 16:33:15 +0100

    Fix event label width calculation

--- js/event-label-width.js
+++ js/event-label-width.js
@@ -1,15 +1,22 @@
+// Match all event meta labels to the width of the widest label
 jQuery(function ($) {
     var $labels = $('.event-listing-meta .label');
     if (!$labels.length) {
         return;
     }
+
     var maxWidth = 0;
     $labels.each(function () {
-        var width = $(this).outerWidth();
+        var $label = $(this);
+        var originalMin = $label.css('min-width');
+        $label.css('min-width', 0);
+        var width = $label.outerWidth();
+        $label.css('min-width', originalMin);
         if (width > maxWidth) {
             maxWidth = width;
         }
     });
+
     maxWidth = Math.ceil(maxWidth / 5) * 5;
     $labels.css({
         width: maxWidth + 'px',
