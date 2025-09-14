2b565619230b460e78b16fbdb7809c61e072abdd
rvwest <github@rvwest.co.uk>
2025-09-14 12:24:05 +0100

    viewports - home

--- _home-jobs-item.php
+++ _home-jobs-item.php
@@ -6,9 +6,10 @@ echo '<!-- file: _home-jobs-item.php -->'; ?>
 <article <?php post_class('article-item job-summary'); ?>>
 <a href="<?php the_permalink(); ?>" class="crp_link post-3212 crp_job">
 	<?php if ( $logo = get_the_company_logo() ) : ?>
+		<div class="home-article--image">
 		<div class="home-article-image">
 			<img src="<?php echo esc_url( $logo ); ?>" alt="<?php the_company_name(); ?>" title="<?php the_company_name(); ?>" class="crp_thumb crp_featured crp_job_logo" />
-	</div>
+	</div></div>
 	<?php endif; ?>
 	<h3 class="crp_title">
 		<span class="title-tag"><i class="far fa-bell-on"></i> Job: </span>
