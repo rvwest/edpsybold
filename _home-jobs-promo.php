a4e757ee3264e801d160c1f67d31acaa36c2dc2f
rvwest <github@rvwest.co.uk>
2025-09-14 17:58:57 +0100

    viewport - home - rc

--- _home-jobs-promo.php
+++ _home-jobs-promo.php
@@ -4,17 +4,29 @@ if (!defined('ABSPATH')) {
 }
 echo '<!-- file: _home-jobs-promo.php -->'; ?>
 <article <?php post_class('article-item job-summary page-promo '); ?>>
-	<div class=" home-article-image">
-			<a href="jobs/post" class="crp_link crp_job edp-jobs-promo--home edp-jobs-promo--ad ">
+<a href="jobs/post" class="crp_link crp_job edp-jobs-promo--home edp-jobs-promo--ad "><div class="article-item--image">
+	<div class="home-article-image">
+			
 			<div class="promo-text">
 			<div class="promo-title">Add your job</div>
-			<div class="promo-subtitle">£400 for 30 days
+			<div class="promo-subtitle">
 			+ mailout and social promos</div>
 			</div>
 			<i class="fal fa-plus-circle fa-2x"></i>
-			</a>
-	</div>
+			
+	</div></div>
+	<h3 class="crp_title">
+		<div class="promo-oneline">£400 for 30 days</div>
+		<div class="promo-twolines">Add your job</div>
+		
+			<div class="promo-subtitle"><span class="promo-twolines">£400 for 30 days </span>+ mailout and social promos</div>
+			</div>
+			
+	</h3>
+	
+	</a>
+</article>
+
 	
 
-</article>
 
