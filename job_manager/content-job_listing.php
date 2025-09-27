63a8d1b43b2734ad40e8db860ab9ed4c81376740
rvwest <github@rvwest.co.uk>
2025-09-27 18:52:19 +0100

    jobs - small vp - index

--- job_manager/content-job_listing.php
+++ job_manager/content-job_listing.php
@@ -1,7 +1,7 @@
 <!-- file: job_manager/content-job_listing.php -->
 
-<?php if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
+<?php if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
 }
 
 echo '<!-- file: job_manager/content-job_listing.php -->';
@@ -11,37 +11,39 @@ echo '<!-- file: job_manager/content-job_listing.php -->';
     data-latitude="<?php echo esc_attr($post->geolocation_lat); ?>">
     <a href="<?php the_job_permalink(); ?>">
         <div class="job-listing-logo"><?php the_company_logo(); ?></div>
-
-        <div class="job-listing-details">
-            <div class="job-title">
-                <h2><?php wpjm_the_job_title(); ?></h2>
-            </div>
-
-            <div class="company">
-                <?php the_company_name(); ?>
-
-            </div>
-        </div>
-        <div class="job-listing-meta">
-            <!-- <span class="location"></span> -->
-
-
-            <?php do_action('job_listing_meta_start'); ?>
-            <?php if (get_option('job_manager_enable_types')) { ?>
-                <div class="meta-types">
-                    <?php $types = wpjm_get_the_job_types(); ?>
-                    <?php if (!empty($types)):
-                        foreach ($types as $type): ?>
-                            <span
-                                class="job-type <?php echo esc_attr(sanitize_title($type->slug)); ?>"><?php echo esc_html($type->name); ?></span>
-                        <?php endforeach; endif; ?>
+        <div class="job-listing--details-meta">
+            <div class="job-listing-details">
+                <div class="job-title">
+                    <h2><?php wpjm_the_job_title(); ?></h2>
                 </div>
-            <?php } ?>
-            <div class="salary"><?php gma_wpjmef_display_combined_data_listings(); ?></div>
 
+                <div class="company">
+                    <?php the_company_name(); ?>
+
+                </div>
+            </div>
+            <div class="job-listing-meta">
+                <!-- <span class="location"></span> -->
+
+
+                <?php do_action('job_listing_meta_start'); ?>
+                <?php if (get_option('job_manager_enable_types')) { ?>
+                    <div class="meta-types">
+                        <?php $types = wpjm_get_the_job_types(); ?>
+                        <?php if (!empty($types)):
+                            foreach ($types as $type): ?>
+                                <span
+                                    class="job-type <?php echo esc_attr(sanitize_title($type->slug)); ?>"><?php echo esc_html($type->name); ?></span>
+                            <?php endforeach; endif; ?>
+                    </div>
+                <?php } ?>
+                <div class="salary"><?php gma_wpjmef_display_combined_data_listings(); ?></div>
+
+            </div>
+
+
+            <?php do_action('job_listing_meta_end'); ?>
         </div>
-
-        <?php do_action('job_listing_meta_end'); ?>
         </div>
     </a>
 </li>
