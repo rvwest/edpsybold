40d1dcc4e2f6a23b6baf7b64856241f1afd77c5b
rvwest <github@rvwest.co.uk>
2025-10-09 15:27:53 +0100

    hero linking issue

--- _home-slice-hero.php
+++ _home-slice-hero.php
@@ -1,6 +1,6 @@
-<?php 
+<?php
 if (!defined('ABSPATH')) {
-	exit; // Exit if accessed directly.
+    exit; // Exit if accessed directly.
 }
 echo '<!-- file: home-slice-hero.php -->';
 $hero_post_id = get_theme_mod('hero_post_id');
@@ -11,26 +11,29 @@ if ($hero_post_id) {
     ?>
 
     <section class="hero-post grid12">
-<h2>Featured</h2>
-    <article <?php post_class('home-hero-item'); ?>>
-<a href="<?php the_permalink(); ?>">
-<div class="hero-text"><h1><?php echo esc_html(get_the_title($hero_post)); ?></h1>
-<?php
-$raw_content = apply_filters('the_content', $hero_post->post_content);
-$paragraphs = explode('</p>', $raw_content);
+        <h2>Featured</h2>
+        <article <?php post_class('home-hero-item', $hero_post_id); ?>>
+            <a href="<?php echo esc_url(get_permalink($hero_post_id)); ?>">
+                <div class="hero-text">
+                    <h1><?php echo esc_html(get_the_title($hero_post)); ?></h1>
+                    <?php
+                    $raw_content = apply_filters('the_content', $hero_post->post_content);
+                    $paragraphs = explode('</p>', $raw_content);
 
-// Get the first paragraph (if it exists)
-if (!empty($paragraphs[0])) {
-    echo '<div class="hero-excerpt">' . $paragraphs[0] . '</p></div>';
-}
-?> </div>  
-<?php if ($hero_image): ?>
-    <div class="hero-image" style="background-image: url(<?php echo esc_url($hero_image); ?>" alt="<?php echo esc_attr(get_the_title($hero_post)); ?>">
-            </div>
-        <?php endif; ?>
-        
-        </a>
-    </article>
+                    // Get the first paragraph (if it exists)
+                    if (!empty($paragraphs[0])) {
+                        echo '<div class="hero-excerpt">' . $paragraphs[0] . '</p></div>';
+                    }
+                    ?>
+                </div>
+                <?php if ($hero_image): ?>
+                    <div class="hero-image" style="background-image: url('<?php echo esc_url($hero_image); ?>')"
+                        alt=" <?php echo esc_attr(get_the_title($hero_post)); ?>">
+                    </div>
+                <?php endif; ?>
+
+            </a>
+        </article>
     </section>
     <?php
 }
