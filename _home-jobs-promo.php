5ba09636a77d44dc9aebf1f1455778874cb8c687
rvwest <github@rvwest.co.uk>
2025-11-09 20:55:34 +0000

    improved jobs promo

--- _home-jobs-promo.php
+++ _home-jobs-promo.php
@@ -1,32 +1,31 @@
-<?php 
+<?php
 if (!defined('ABSPATH')) {
 	exit; // Exit if accessed directly.
 }
 echo '<!-- file: _home-jobs-promo.php -->'; ?>
 <article <?php post_class('article-item job-summary page-promo '); ?>>
-<a href="jobs/post" class="crp_link crp_job edp-jobs-promo--home edp-jobs-promo--ad "><div class="article-item--image">
-	<div class="home-article-image">
-			
-			<div class="promo-text">
-			<div class="promo-title">Add your job</div>
-			<div class="promo-subtitle">
-			+ mailout and social promos</div>
+	<a href="jobs/post" class="crp_link crp_job edp-jobs-promo--home edp-jobs-promo--ad ">
+		<div class="article-item--image">
+			<div class="home-article-image">
+
+				<div class="promo-text">
+					<div class="promo-title">Add your job</div>
+					<div class="promo-subtitle">
+						+ mailout and social promos</div>
+				</div>
+				<i class="fal fa-plus-circle fa-2x"></i>
+
 			</div>
-			<i class="fal fa-plus-circle fa-2x"></i>
-			
-	</div></div>
-	<h3 class="crp_title">
-		<div class="promo-oneline">£400 for 30 days</div>
-		<div class="promo-twolines">Add your job</div>
-		
-			<div class="promo-subtitle"><span class="promo-twolines">£400 for 30 days </span>+ mailout and social promos</div>
+		</div>
+		<h3 class="crp_title">
+			<div class="promo-oneline">£400 for 30 days</div>
+			<div class="promo-twolines">Add your job</div>
+
+			<div class="promo-subtitle"><span class="promo-twolines">£400 for 30 days </span>+ mailout and social promos
 			</div>
-			
-	</h3>
-	
+
+
+		</h3>
+
 	</a>
 </article>
-
-	
-
-
